4c917c6774c7607852821423b598acdb2caeeff4
cheng.cheng@haoqitech.com.cn <ChengCheng1234>
2017-11-30 00:09:30 +0800

    代码提交

--- shopDemo/routes/back.js
+++ shopDemo/routes/back.js
@@ -4,8 +4,17 @@ var userModel = require('../model/user.js');
 
 /* GET users listing. */
 /* / */
-router.get('/', function(req, res, next) {
-  res.send('back')
+/*主页面 iframe*/
+router.get('/index', function(req, res, next) {
+  res.render('indexBack',{
+
+  })
+});
+/*修改轮播图*/
+router.get('/changeCarousel', function(req, res, next) {
+  res.render('homepage/changeCarousel',{
+
+  })
 });
 
 module.exports = router;
