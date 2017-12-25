59e3e32657061af6f1299f99afd5996eda5f8239
chengcheng <chengcheng@haoqitech.com.cn>
2017-12-25 18:52:18 +0800

    提交代码

--- shopDemo/resource/src/main.js
+++ shopDemo/resource/src/main.js
@@ -1,9 +1,13 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import App from './App'
 import router from './router'
 import axios from 'axios'
+
+Vue.use(ElementUI, { size: 'small' })
 
 Vue.config.productionTip = false
 Vue.prototype.$http = axios
@@ -12,6 +16,7 @@ Vue.prototype.$http = axios
 new Vue({
   el: '#app',
   router,
+  render: h => h(App),
   template: '<App/>',
   components: { App }
 })
